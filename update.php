33f1ea33bb3aa041992540fd61446b87f137559a
Kwan6622 <nguyenquang6216@gmail.com>
2024-04-05 10:30:02 +0700

    Update update.php

--- update.php
+++ update.php
@@ -11,33 +11,33 @@ if (!$con) {
 }
 
 // Define variables and initialize with empty values
-$update_id_number = trim($_POST['update_id_number']);
-$update_status = $_POST['update_status'];
 
-// Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Validate EOI
-  if (empty(trim($_POST["update_id_number"]))) {
-    $update_id_number_err = "Please enter EOI.";
+
+else{
+
+  $update_id_number = trim($_POST['update_id_number']);
+  $update_status = $_POST['update_status'];
+
+
+  if (empty($_POST["update_id_number"])) {
+    echo "Please enter EOI.";
   } else {
     $update_id_number = trim($_POST["update_id_number"]);
+    $update_query = "UPDATE Applicant SET Status = '$update_status' WHERE EOI = '$update_id_number'";
+    $update_status = $_POST['update_status'];
+    // Update the status in the database
+    
+      if (mysqli_query($con, $update_query)) {
+        echo '<a href="manage.php">Back</a>';
+        echo "<p>Status updated successfully!</p>";
+      } else {
+        echo '<a href="manage.php">Back</a>';
+        echo "<p>Error updating status: </p>" . mysqli_error($con);
+      }
+  }
   }
 
   // Validate input
-  $update_status = $_POST['update_status'];
-
-  // Check if EOI is valid
-  if (empty($update_id_number_err)) {
-    // Update the status in the database
-    $update_query = "UPDATE Applicant SET Status = '$update_status' WHERE EOI = '$update_id_number'";
-    if (mysqli_query($con, $update_query)) {
-      echo '<a href="manage.php">Back</a>';
-      echo "<p>Status updated successfully!</p>";
-    } else {
-      echo '<a href="manage.php">Back</a>';
-      echo "<p>Error updating status: </p>" . mysqli_error($con);
-    }
-  }
-}
+  
 
 ?>
